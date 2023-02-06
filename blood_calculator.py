9f06f6ca540a6ced9a9e925a6e431e5cb9b368f5
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-02-06 13:35:12 -0500

    Changed inputs and output for each test to generic_input and generic_output, and added spacing between functions for formatting

--- blood_calculator.py
+++ blood_calculator.py
@@ -19,39 +19,28 @@ def interface():
     print("Program Ending")
 
 def HDL_driver():
-    HDL_in = HDL_input()
+    HDL_in = generic_input("HDL")
     HDL_analy = HDL_analysis(HDL_in)
-    HDL_output(HDL_in, HDL_analy)
+    generic_output("HDL", HDL_in, HDL_analy)
+
+
+def generic_input(test_name):
+    value = input("Enter the {} value:".format(test_name))
+    value = int(value)
+    return value
+
 
 def LDL_driver():
-    LDL_in = LDL_input()
+    LDL_in = generic_input("LDL")
     LDL_analy = LDL_analysis(LDL_in)
-    LDL_output(LDL_in, LDL_analy)
+    generic_output("LDL", LDL_in, LDL_analy)
+
 
 def chol_driver():
-    chol_in = chol_input()
+    chol_in = generic_input("Cholesterol")
     chol_analy = chol_analysis(chol_in)
-    chol_output(chol_in, chol_analy)
+    generic_output("Cholesterol", chol_in, chol_analy)
 
-def HDL_input():
-    HDL_value = input("Enter the HDL result:")
-    HDL_value = int(HDL_value)
-    return HDL_value
-
-def LDL_input():
-    LDL_value = input("Enter the LDL result:")
-    LDL_value = int(LDL_value)
-    return LDL_value
-
-def chol_input():
-    chol_value = input("Enter the Cholesterol result:")
-    chol_value = int(chol_value)
-    return chol_value
-    
-def chol_input():
-    chol_value = input("Enter the Cholesterol result:")
-    chol_value = int(chol_value)
-    return chol_value
 
 def HDL_analysis(HDL_int):
     if HDL_int >= 60:
@@ -61,6 +50,7 @@ def HDL_analysis(HDL_int):
     else:
         answer = "Low"
     return answer
+
 
 def LDL_analysis(LDL_int):
     if LDL_int < 130:
@@ -73,6 +63,7 @@ def LDL_analysis(LDL_int):
         answer  = "Very High"
     return answer
 
+
 def chol_analysis(chol_int):
     if chol_int < 200:
         answer = "Normal"
@@ -82,17 +73,13 @@ def chol_analysis(chol_int):
         answer = "High"
     return answer
 
-def HDL_output(HDL_value, HDL_analy):
-    print("The HDL result of {} is considered {}".format(HDL_value, HDL_analy))
-    return
 
-def LDL_output(LDL_value, LDL_analy):
-    print("The LDL result of {} is considered {}".format(LDL_value, LDL_analy))
-    return
+def generic_output(test_name, test_value, test_analy):
+    print("The {} result of {} is considered {}"
+        .format(test_name, test_value, test_analy))
+    return 
 
-def chol_output(chol_value, chol_analy):
-    print("The LDL result of {} is considered {}".format(chol_value, chol_analy))
-    return
+
 
 if __name__ == "__main__":
     interface()
