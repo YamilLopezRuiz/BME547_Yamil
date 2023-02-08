cd6c8884be97a7a1ea066032426bf847a0c72e05
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-02-08 12:57:21 -0500

    Added files comprehensive_testing_example, add, and test_add which were done in class on 2/8/2023

---
+++ comprehensive_testing_example.py
@@ -0,0 +1,39 @@
+import math
+
+def is_adult(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+
+"""
+(25, True)
+(18, True)
+(10, False)
+"""
+
+def area_of_ellpse(x: list):
+    """ CAlculate the area of an ellipse based on the various inputs
+
+        
+    """
+    data = x.split(",")
+    if len(data) == 1:
+        area = math.pi * data[0]**2
+    elif len(data) == 2:
+        area = math.pi * data[0] * data[1]
+    elif len(data) == 4:
+        a = (data[2] - data[0]) /2
+        b = (data[3] - data[1]) /2
+        area = math.pi * a * b
+    else:
+        area = None
+    return area
+
+"""
+([1,5], area)
+([1.5, [2.5]], area)
+([1, 10, 20, 30], area)
+([1, 2, 3], area)
+([], None)
+"""
