ba9518d7a48de259f8cc9511d4105f9ec60ad916
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-02-17 17:10:40 -0500

    Corrected syntax again

--- test_Feb03_assignment.py
+++ test_Feb03_assignment.py
@@ -7,8 +7,6 @@ import pytest
  ([2, 13], [8, 37], 45, 185),  # Letters and numbers
  ([-3, 28], [5, -12], 6, -17)  # Letters and dash
  ])
-
-
 def test_find_third_point(x, y, test, expected):
     from Feb03_assignment import find_third_point
     answer = find_third_point(x, y, test)
