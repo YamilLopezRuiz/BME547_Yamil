f6f3956427c4cc8c3caa3971dd08c7c78dcff943
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-02-17 17:24:32 -0500

    Added another function that checks if a new pair of x y matches with two given sets

--- test_Feb03_assignment.py
+++ test_Feb03_assignment.py
@@ -11,3 +11,15 @@ def test_find_third_point(x, y, test, expected):
     from Feb03_assignment import find_third_point
     answer = find_third_point(x, y, test)
     assert answer == expected
+
+
+@pytest.mark.parametrize("first, second, third, expected", [
+ ([1, 1], [2, 2], [3, 3], True),  # Only letters
+ ([1, 1], [2, 2], [3, 2], False),  # Only numbers
+ ([-3, 28], [5, -12], [6, -17], True),  # Letters and numbers
+ ([-3, 28], [5, -12], [2, -17], False)  # Letters and dash
+ ])
+def test_check_new_point(first, second, third, expected):
+    from Feb03_assignment import check_new_point
+    answer = check_new_point(first, second, third)
+    assert answer == expected
