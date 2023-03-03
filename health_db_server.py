1ccf7be949e27f58c6df810b99479b5c26d799f7
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-03-03 13:13:46 -0500

    began to create health_db_server.py

---
+++ health_db_server.py
@@ -0,0 +1,11 @@
+"""
+Database = Dictionary
+keys -> ids for the patients
+value: int
+
+{1: {"id": 1, "name": "David", "blood_type": "O+"},
+ 2: {"id": 2, "name": "Riddhi", "blood_type": "A+"},
+ 3: {"id": 3, "name": "Daniel", "blood_type": "B-", "tests": []}
+}
+
+"""
