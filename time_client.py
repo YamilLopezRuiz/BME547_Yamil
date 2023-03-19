1f7c18cedbee0c0bab1c0931c2d64115d4a16efa
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-03-19 18:52:39 -0400

    Made changes for pycodestyle

--- time_client.py
+++ time_client.py
@@ -1,4 +1,3 @@
-import datetime
 import requests
 
 server = "http://127.0.0.1:5000"
@@ -12,11 +11,11 @@ print(r.status_code)
 print(r.text)
 
 age = {'date': "10-10-1999", 'units': "years"}
-r = requests.post(server+ "/age", json=age)
+r = requests.post(server + "/age", json=age)
 print(r.status_code)
 print(r)
 print(r.json())
 
-r = requests.get(server + "/until_next_meal/dinner")
+r = requests.get(server + "/until_next_meal/breakfast")
 print(r.status_code)
 print(r.json())
--- time_server.py
+++ time_server.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
-import datetime
+from datetime import datetime
+
 
 app = Flask(__name__)
+
+
 def find_datetime():
-    current = datetime.datetime.now()
+    current = datetime.now()
     return current
+
 
 @app.route("/time", methods=["GET"])
 def find_time():
@@ -24,13 +28,14 @@ def find_date():
 def add_handler():
     in_data = request.get_json()  # string
     input_date = in_data['date']
-    input_date = datetime.datetime.strptime(input_date, "%m-%d-%Y")
+    input_date = datetime.strptime(input_date, "%m-%d-%Y")
     current_date = find_datetime()  # Current date
     date_diff = current_date - input_date
     difference = date_diff.days
     year_diff = difference / 365
     year_diff = round(year_diff, 2)
     return jsonify(year_diff)
+
 
 @app.route("/until_next_meal/<meal>", methods=["GET"])
 def find_until_next_meal(meal):
@@ -44,7 +49,7 @@ def find_until_next_meal(meal):
     elif meal == "dinner":
         mealtime = current_date + " 20:0:0"
     print(mealtime)
-    mealtime = datetime.datetime.strptime(mealtime, "%m-%d-%Y %H:%M:%S")
+    mealtime = datetime.strptime(mealtime, "%m-%d-%Y %H:%M:%S")
     time_diff = mealtime - current_time
     difference = time_diff.seconds
     meal_time = difference / 60
