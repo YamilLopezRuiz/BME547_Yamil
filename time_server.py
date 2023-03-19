af352ec7f8af96cb08bc54059ef3e2040dfd40e4
Yamil Lopez-Ruiz <lopez.ruiz.y@gmail.com>
2023-03-19 12:19:20 -0400

    Added datetime to requirements.txt and installed, added to time_server,py

--- time_server.py
+++ time_server.py
@@ -1,7 +1,17 @@
 from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/get_results/<patient_id>", methods=["GET"])
+@app.route("/get_time/<patient_id>", methods=["GET"])
 def find_time():
     return
+
+
+@app.route("/get_date/<patient_id>", methods=["GET"])
+def find_date():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
